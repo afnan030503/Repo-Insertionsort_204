1d6ca9d40e759ed3d27c33d7302e4daae4d1ee38
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-03-24 10:15:50 +0700

    Add project files.

---
+++ insertsort/insertsort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+   
+void input() {
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data Array: ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+}
